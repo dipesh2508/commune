f415fb76b66e5c59512f56e674dce301e50ad56e
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-01-09 02:23:38 +0530

    added button ui component

--- components/forms/AuthForm.tsx
+++ components/forms/AuthForm.tsx
@@ -3,7 +3,8 @@
 import { useCallback, useState } from "react";
 import { FieldValues, useForm, SubmitHandler } from "react-hook-form";
 
-import Input from '@/components/inputs/Input'
+import Input from "@/components/ui/Input";
+import Button from "@/components/ui/Button";
 
 type Variant = "LOGIN" | "REGISTER";
 
@@ -32,33 +33,48 @@ const AuthForm = () => {
     },
   });
 
-const onSubmit: SubmitHandler<FieldValues> = (data) => {
+  const onSubmit: SubmitHandler<FieldValues> = (data) => {
     setIsLoading(true);
 
     if (variant === "REGISTER") {
       // REGISTER
-    } 
+    }
 
     if (variant === "LOGIN") {
       // LOGIN
     }
-  }
+  };
 
-  const socialAction = (action: string) =>{
+  const socialAction = (action: string) => {
     setIsLoading(true);
 
     //Social Sign in
-  }
+  };
 
   return (
     <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
-        <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
-            <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
-                <Input id="email" label='Email' register={register} errors={errors} />
-            </form>
-        </div>
+      <div className="bg-white px-4 py-8 shadow sm:rounded-lg sm:px-10">
+        <form className="space-y-6" onSubmit={handleSubmit(onSubmit)}>
+          {variant === "REGISTER" && (
+            <Input id="name" label="Name" register={register} errors={errors} />
+          )}
+          <Input id="email" label="Email" type="email" register={register} errors={errors} />
+          <Input
+            id="password"
+            label="Password"
+            type="password"
+            register={register}
+            errors={errors}
+          />
+          <div>
+            <Button disabled={isLoading} fullWidth type="submit">
+              {variant === "LOGIN" ? "Sign in" : "Sign up"}
+            </Button>
+          </div>
+        </form>
+      </div>
     </div>
-  )
+  );
 };
 
 export default AuthForm;
