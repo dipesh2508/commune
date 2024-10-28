bde701afae5e8d7e3cbfc520bfac5616f3ee5134
Dipesh Ranjan <90145046+dipesh2508@users.noreply.github.com>
2024-10-28 17:06:48 +0530

    feat: enhance message timestamp display

--- components/shared/MessageBox.tsx
+++ components/shared/MessageBox.tsx
@@ -4,7 +4,7 @@ import { FullMessageType } from "@/types";
 import clsx from "clsx";
 import { useSession } from "next-auth/react";
 import Avatar from "../Avatar";
-import { format } from "date-fns";
+import { format, isToday, isYesterday } from "date-fns";
 import Image from "next/image";
 import { useState } from "react";
 import ImageModal from "./ImageModal";
@@ -36,6 +36,12 @@ const MessageBox: React.FC<MessageBoxProps> = ({ isLast, data }) => {
     data.image ? "rounded-md p-0" : "rounded-full py-2 px-3",
   );
 
+  const timeFormat = isToday(new Date(data.createdAt))
+    ? format(new Date(data.createdAt), "p")
+    : isYesterday(new Date(data.createdAt))
+      ? "Yesterday"
+      : format(new Date(data.createdAt), "MMM d, yyyy");
+
   return (
     <div className={container}>
       <div className={avatar}>
@@ -44,30 +50,30 @@ const MessageBox: React.FC<MessageBoxProps> = ({ isLast, data }) => {
       <div className={body}>
         <div className="flex items-center gap-1">
           <div className="text-sm text-gray-500">{data.sender.name}</div>
-          <div className="text-xs text-gray-400">
-            {format(new Date(data.createdAt), "p")}
-          </div>
+          <div className="text-xs text-gray-400">{timeFormat}</div>
         </div>
         <div className={message}>
-          <ImageModal src={data.image} isOpen={imageModalOpen} onClose={() => setImageModalOpen(false)} />
-          
-            {data.image ? (
-                <Image
-                onClick={() => setImageModalOpen(true)}
-                alt="Image"
-                height={288}
-                width={288}
-                src={data.image}
-                className="object-cover cursor-pointer hover:scale-110 transition translate"
-                />
-            ): (
-                <div>
-                    {data.body}
-                </div>
-            )}
+          <ImageModal
+            src={data.image}
+            isOpen={imageModalOpen}
+            onClose={() => setImageModalOpen(false)}
+          />
+
+          {data.image ? (
+            <Image
+              onClick={() => setImageModalOpen(true)}
+              alt="Image"
+              height={288}
+              width={288}
+              src={data.image}
+              className="translate cursor-pointer object-cover transition hover:scale-110"
+            />
+          ) : (
+            <div>{data.body}</div>
+          )}
         </div>
         {isLast && isOwn && seenList.length > 0 && (
-          <div className="text-xs fon-light text-gray-500">
+          <div className="fon-light text-xs text-gray-500">
             {`Seen by ${seenList}`}
           </div>
         )}
